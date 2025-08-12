3fa1af83046f8d67e9e35d55b9338b80e96ca856
Errorsia <Errorsia@outlook.com>
2025-08-12 20:23:25 +0800

    version 2 - alpha

--- morse_translator.py
+++ morse_translator.py
@@ -1,26 +1,3 @@
-# import sys
-#
-#
-# class MorseCode:
-#     def __init__(self):
-#         input_string = input()
-#         if not self.is_valid_code(input_string):
-#             sys.exit('Unsupported format')
-#
-#
-#     @staticmethod
-#     def get_elements(s):
-#         chars = s.replace(' ', '')
-#         unique_chars = set(chars)
-#         return unique_chars
-#
-#
-#     @staticmethod
-#     def is_valid_code(s):
-#         chars = s.replace(' ', '')
-#         unique_chars = set(chars)
-#         return len(unique_chars) == 1 or len(unique_chars) == 2
-#
 # 定义莫尔斯电码字典
 # 打表法还是太强了
 MORSE_CODE_DICT = {
